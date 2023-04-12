7752bc1662099372a343c911a24507f535729d78
shibinjohn88 <shibinjohnpdlm@gmail.com>
2023-04-12 19:41:17 -0400

    style sheet changed to sassy

--- script.js
+++ script.js
@@ -58,14 +58,14 @@ function shuffle(ar) {
       // Pick a remaining element.
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
       // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
       }
-    
       return array;
     }
+  
+    
   
 //pick an image from the shuffled image array
 function getBackImage(i) {
